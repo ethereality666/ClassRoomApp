6d5306546b3b8700830001685710b7a9fdbc09d0
ethereality666 <s.kachkurkin2001@gmail.com>
2022-01-31 23:30:51 +0300

    Added function import from mysql

--- ClassRoomApp/Main.Designer.cs
+++ ClassRoomApp/Main.Designer.cs
@@ -29,20 +29,388 @@ namespace ClassRoomApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonExportStudents = new System.Windows.Forms.Button();
+            this.buttonStudentUpdate = new System.Windows.Forms.Button();
+            this.buttonChangeStudInformation = new System.Windows.Forms.Button();
+            this.buttonDeleteStudent = new System.Windows.Forms.Button();
+            this.buttonStudentAdd = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewStudent = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxThirdname = new System.Windows.Forms.TextBox();
+            this.listBoxClass = new System.Windows.Forms.ListBox();
+            this.dateBirthd = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(991, 588);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.buttonExportStudents);
+            this.tabPage1.Controls.Add(this.buttonStudentUpdate);
+            this.tabPage1.Controls.Add(this.buttonChangeStudInformation);
+            this.tabPage1.Controls.Add(this.buttonDeleteStudent);
+            this.tabPage1.Controls.Add(this.buttonStudentAdd);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(983, 562);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ученики";
+            // 
+            // buttonExportStudents
+            // 
+            this.buttonExportStudents.Location = new System.Drawing.Point(902, 530);
+            this.buttonExportStudents.Name = "buttonExportStudents";
+            this.buttonExportStudents.Size = new System.Drawing.Size(75, 23);
+            this.buttonExportStudents.TabIndex = 5;
+            this.buttonExportStudents.Text = "Экспорт";
+            this.buttonExportStudents.UseVisualStyleBackColor = true;
+            // 
+            // buttonStudentUpdate
+            // 
+            this.buttonStudentUpdate.Location = new System.Drawing.Point(821, 530);
+            this.buttonStudentUpdate.Name = "buttonStudentUpdate";
+            this.buttonStudentUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonStudentUpdate.TabIndex = 4;
+            this.buttonStudentUpdate.Text = "Обновить";
+            this.buttonStudentUpdate.UseVisualStyleBackColor = true;
+            this.buttonStudentUpdate.Click += new System.EventHandler(this.buttonStudentUpdate_Click);
+            // 
+            // buttonChangeStudInformation
+            // 
+            this.buttonChangeStudInformation.Location = new System.Drawing.Point(169, 530);
+            this.buttonChangeStudInformation.Name = "buttonChangeStudInformation";
+            this.buttonChangeStudInformation.Size = new System.Drawing.Size(75, 23);
+            this.buttonChangeStudInformation.TabIndex = 3;
+            this.buttonChangeStudInformation.Text = "Изменить";
+            this.buttonChangeStudInformation.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteStudent
+            // 
+            this.buttonDeleteStudent.Location = new System.Drawing.Point(88, 530);
+            this.buttonDeleteStudent.Name = "buttonDeleteStudent";
+            this.buttonDeleteStudent.Size = new System.Drawing.Size(75, 23);
+            this.buttonDeleteStudent.TabIndex = 2;
+            this.buttonDeleteStudent.Text = "Удалить";
+            this.buttonDeleteStudent.UseVisualStyleBackColor = true;
+            // 
+            // buttonStudentAdd
+            // 
+            this.buttonStudentAdd.Location = new System.Drawing.Point(7, 530);
+            this.buttonStudentAdd.Name = "buttonStudentAdd";
+            this.buttonStudentAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonStudentAdd.TabIndex = 1;
+            this.buttonStudentAdd.Text = "Добавить";
+            this.buttonStudentAdd.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBoxPhone);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.dateBirthd);
+            this.groupBox2.Controls.Add(this.listBoxClass);
+            this.groupBox2.Controls.Add(this.textBoxThirdname);
+            this.groupBox2.Controls.Add(this.textBoxSurname);
+            this.groupBox2.Controls.Add(this.textBoxName);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(7, 352);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(970, 172);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Управление данными";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridViewStudent);
+            this.groupBox1.Location = new System.Drawing.Point(7, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(970, 339);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные об учениках";
+            // 
+            // dataGridViewStudent
+            // 
+            this.dataGridViewStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudent.Location = new System.Drawing.Point(6, 19);
+            this.dataGridViewStudent.Name = "dataGridViewStudent";
+            this.dataGridViewStudent.Size = new System.Drawing.Size(958, 314);
+            this.dataGridViewStudent.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(983, 562);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Оценки";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(983, 562);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Учителя";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(983, 562);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Льготы и пособия";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя :";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(112, 17);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(112, 43);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSurname.TabIndex = 2;
+            // 
+            // textBoxThirdname
+            // 
+            this.textBoxThirdname.Location = new System.Drawing.Point(112, 69);
+            this.textBoxThirdname.Name = "textBoxThirdname";
+            this.textBoxThirdname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxThirdname.TabIndex = 3;
+            // 
+            // listBoxClass
+            // 
+            this.listBoxClass.FormattingEnabled = true;
+            this.listBoxClass.Items.AddRange(new object[] {
+            "1 А",
+            "1 Б",
+            "1 В",
+            "2 А",
+            "2 Б",
+            "2 В",
+            "3 А",
+            "3 Б",
+            "3 В",
+            "4 А",
+            "4 Б",
+            "4 В",
+            "5 А",
+            "5 Б",
+            "5 В",
+            "6 А",
+            "6 Б",
+            "6 В",
+            "7 А",
+            "7 Б",
+            "7 В",
+            "8 А",
+            "8 Б",
+            "8 В",
+            "9 А",
+            "9 Б ",
+            "9 В",
+            "10 А",
+            "10 Б",
+            "11 А"});
+            this.listBoxClass.Location = new System.Drawing.Point(287, 72);
+            this.listBoxClass.Name = "listBoxClass";
+            this.listBoxClass.Size = new System.Drawing.Size(120, 30);
+            this.listBoxClass.TabIndex = 5;
+            // 
+            // dateBirthd
+            // 
+            this.dateBirthd.Location = new System.Drawing.Point(237, 43);
+            this.dateBirthd.Name = "dateBirthd";
+            this.dateBirthd.Size = new System.Drawing.Size(170, 20);
+            this.dateBirthd.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Класс :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Дата рождения :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Номер телефона :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Отчество :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Фамилия : ";
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(112, 95);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPhone.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(112, 146);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Идентификатор : ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(218, 149);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "(для изменения)";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(768, 425);
+            this.ClientSize = new System.Drawing.Size(1015, 612);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Main";
-            this.Text = "Main";
+            this.Text = "Программый комплекс учета учебного процесса \"ЭЛЕКТРОННАЯ ШКОЛА\"";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button buttonExportStudents;
+        private System.Windows.Forms.Button buttonStudentUpdate;
+        private System.Windows.Forms.Button buttonChangeStudInformation;
+        private System.Windows.Forms.Button buttonDeleteStudent;
+        private System.Windows.Forms.Button buttonStudentAdd;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridViewStudent;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateBirthd;
+        private System.Windows.Forms.ListBox listBoxClass;
+        private System.Windows.Forms.TextBox textBoxThirdname;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ClassRoomApp/Main.cs
+++ ClassRoomApp/Main.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +9,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ClassRoomApp
-{
-    public partial class Main : Form
-    {
-        public Main()
-        {
+namespace ClassRoomApp {
+    public partial class Main : Form {
+        public Main() {
             InitializeComponent();
+            updateStudentInfo();
         }
+
+        private void updateStudentInfo() {
+            ConnectionClass connection = new ConnectionClass();
+			MySqlCommand mySqlCommand = new MySqlCommand("SELECT * from `students`,`classes` where `students`.classid = `classes`.id", connection.GetConnection());
+			try {
+				connection.OpenConnection();
+				MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+				dataGridViewStudent.Columns.Add("firstname", "Имя");
+				dataGridViewStudent.Columns.Add("secondname", "Фамилия");
+				dataGridViewStudent.Columns.Add("thirdname", "Имя");
+				dataGridViewStudent.Columns.Add("birthday", "Дата рождения");
+				dataGridViewStudent.Columns.Add("name", "Класс");
+				dataGridViewStudent.Columns.Add("numberphone", "Номер телефона");
+				while (mySqlDataReader.Read())
+				{
+					dataGridViewStudent.Rows.Add(mySqlDataReader["FirstName"].ToString(), mySqlDataReader["SecondName"].ToString());
+				}
+			}
+			catch (Exception ex) {
+				MessageBox.Show(ex.ToString(), "Ошибка!");
+			}
+			finally {
+				connection.CloseConnection();
+			}
+		}
+		private void buttonStudentUpdate_Click(object sender, EventArgs e) {
+			dataGridViewStudent.Columns.Remove("firstname");
+			dataGridViewStudent.Columns.Remove("secondname");
+			dataGridViewStudent.Columns.Remove("thirdname");
+			dataGridViewStudent.Columns.Remove("birthday");
+			dataGridViewStudent.Columns.Remove("name");
+			dataGridViewStudent.Columns.Remove("numberphone");
+			updateStudentInfo();
+			MessageBox.Show("Информация успешно обновлена","Информация");
+		}
     }
 }
