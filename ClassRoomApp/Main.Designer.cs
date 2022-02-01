f6b007123dda673f912ddcde5d2c2464c94c91b1
ethereality666 <s.kachkurkin2001@gmail.com>
2022-02-01 06:27:14 +0300

    added code for marks

--- ClassRoomApp/Main.Designer.cs
+++ ClassRoomApp/Main.Designer.cs
@@ -56,11 +56,31 @@ namespace ClassRoomApp
             this.textBoxStudentID = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.buttonExportMark = new System.Windows.Forms.Button();
+            this.buttonUpdateMark = new System.Windows.Forms.Button();
+            this.buttonDeleteMark = new System.Windows.Forms.Button();
+            this.buttonAddMark = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewMarks = new System.Windows.Forms.DataGridView();
+            this.listBoxSchoolObjects = new System.Windows.Forms.ListBox();
+            this.numericUpDownMark = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownStudentID = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMarks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMark)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStudentID)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -148,9 +168,9 @@ namespace ClassRoomApp
             this.groupBox2.Controls.Add(this.textBoxSurname);
             this.groupBox2.Controls.Add(this.textBoxName);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(7, 261);
+            this.groupBox2.Location = new System.Drawing.Point(7, 259);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(756, 172);
+            this.groupBox2.Size = new System.Drawing.Size(756, 177);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Управление данными";
@@ -158,7 +178,7 @@ namespace ClassRoomApp
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.dataGridViewStudent);
-            this.groupBox1.Location = new System.Drawing.Point(7, 7);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(756, 248);
             this.groupBox1.TabIndex = 0;
@@ -176,10 +196,16 @@ namespace ClassRoomApp
             // tabPage2
             // 
             this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.buttonExportMark);
+            this.tabPage2.Controls.Add(this.buttonUpdateMark);
+            this.tabPage2.Controls.Add(this.buttonDeleteMark);
+            this.tabPage2.Controls.Add(this.buttonAddMark);
+            this.tabPage2.Controls.Add(this.groupBox3);
+            this.tabPage2.Controls.Add(this.groupBox4);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(983, 562);
+            this.tabPage2.Size = new System.Drawing.Size(768, 471);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Оценки";
             // 
@@ -187,7 +213,7 @@ namespace ClassRoomApp
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(983, 562);
+            this.tabPage3.Size = new System.Drawing.Size(768, 471);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Учителя";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -196,7 +222,7 @@ namespace ClassRoomApp
             // 
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(983, 562);
+            this.tabPage4.Size = new System.Drawing.Size(768, 471);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Льготы и пособия";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -348,9 +374,174 @@ namespace ClassRoomApp
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(218, 149);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.Size = new System.Drawing.Size(81, 13);
             this.label8.TabIndex = 15;
-            this.label8.Text = "(для изменения)";
+            this.label8.Text = "(для удаления)";
+            // 
+            // buttonExportMark
+            // 
+            this.buttonExportMark.Location = new System.Drawing.Point(606, 441);
+            this.buttonExportMark.Name = "buttonExportMark";
+            this.buttonExportMark.Size = new System.Drawing.Size(75, 23);
+            this.buttonExportMark.TabIndex = 11;
+            this.buttonExportMark.Text = "Экспорт";
+            this.buttonExportMark.UseVisualStyleBackColor = true;
+            this.buttonExportMark.Click += new System.EventHandler(this.buttonExportMark_Click);
+            // 
+            // buttonUpdateMark
+            // 
+            this.buttonUpdateMark.Location = new System.Drawing.Point(689, 441);
+            this.buttonUpdateMark.Name = "buttonUpdateMark";
+            this.buttonUpdateMark.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdateMark.TabIndex = 10;
+            this.buttonUpdateMark.Text = "Обновить";
+            this.buttonUpdateMark.UseVisualStyleBackColor = true;
+            this.buttonUpdateMark.Click += new System.EventHandler(this.buttonUpdateMark_Click);
+            // 
+            // buttonDeleteMark
+            // 
+            this.buttonDeleteMark.Location = new System.Drawing.Point(87, 441);
+            this.buttonDeleteMark.Name = "buttonDeleteMark";
+            this.buttonDeleteMark.Size = new System.Drawing.Size(75, 23);
+            this.buttonDeleteMark.TabIndex = 9;
+            this.buttonDeleteMark.Text = "Удалить";
+            this.buttonDeleteMark.UseVisualStyleBackColor = true;
+            this.buttonDeleteMark.Click += new System.EventHandler(this.buttonDeleteMark_Click);
+            // 
+            // buttonAddMark
+            // 
+            this.buttonAddMark.Location = new System.Drawing.Point(6, 441);
+            this.buttonAddMark.Name = "buttonAddMark";
+            this.buttonAddMark.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddMark.TabIndex = 7;
+            this.buttonAddMark.Text = "Добавить";
+            this.buttonAddMark.UseVisualStyleBackColor = true;
+            this.buttonAddMark.Click += new System.EventHandler(this.buttonAddMark_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.numericUpDownStudentID);
+            this.groupBox3.Controls.Add(this.numericUpDownMark);
+            this.groupBox3.Controls.Add(this.listBoxSchoolObjects);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Controls.Add(this.label16);
+            this.groupBox3.Location = new System.Drawing.Point(7, 260);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(756, 172);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Управление данными";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(8, 46);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(54, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Оценка : ";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(8, 20);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(97, 13);
+            this.label16.TabIndex = 0;
+            this.label16.Text = "Идентификатор* :";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.dataGridViewMarks);
+            this.groupBox4.Location = new System.Drawing.Point(6, 6);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(756, 248);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Данные об оценках";
+            // 
+            // dataGridViewMarks
+            // 
+            this.dataGridViewMarks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMarks.Location = new System.Drawing.Point(6, 19);
+            this.dataGridViewMarks.Name = "dataGridViewMarks";
+            this.dataGridViewMarks.Size = new System.Drawing.Size(744, 223);
+            this.dataGridViewMarks.TabIndex = 0;
+            // 
+            // listBoxSchoolObjects
+            // 
+            this.listBoxSchoolObjects.FormattingEnabled = true;
+            this.listBoxSchoolObjects.Items.AddRange(new object[] {
+            "Русский язык",
+            "Литература",
+            "Математика",
+            "Информатика",
+            "Алгебра",
+            "Геометрия",
+            "Черчение",
+            "Изобразительное искусство",
+            "Биология",
+            "Физкультура",
+            "История России",
+            "Всеобщая история",
+            "Астрономия",
+            "Физика",
+            "География",
+            "Иностранный язык",
+            "Технология",
+            "Химия",
+            "Музыка",
+            "ОБЖ"});
+            this.listBoxSchoolObjects.Location = new System.Drawing.Point(219, 44);
+            this.listBoxSchoolObjects.Name = "listBoxSchoolObjects";
+            this.listBoxSchoolObjects.Size = new System.Drawing.Size(178, 108);
+            this.listBoxSchoolObjects.TabIndex = 12;
+            // 
+            // numericUpDownMark
+            // 
+            this.numericUpDownMark.Location = new System.Drawing.Point(112, 44);
+            this.numericUpDownMark.Name = "numericUpDownMark";
+            this.numericUpDownMark.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDownMark.TabIndex = 13;
+            this.numericUpDownMark.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownStudentID
+            // 
+            this.numericUpDownStudentID.Location = new System.Drawing.Point(112, 18);
+            this.numericUpDownStudentID.Name = "numericUpDownStudentID";
+            this.numericUpDownStudentID.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDownStudentID.TabIndex = 14;
+            this.numericUpDownStudentID.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(9, 157);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(529, 12);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Идентификатор ученика можно узнать во вкладке \"Ученики\". Для удаления оценки испо" +
+    "льзовать идентификатор оценки!";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(216, 20);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Предмет :";
             // 
             // Main
             // 
@@ -367,6 +558,13 @@ namespace ClassRoomApp
             this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMarks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMark)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStudentID)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -400,6 +598,20 @@ namespace ClassRoomApp
         private System.Windows.Forms.TextBox textBoxSurname;
         private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonExportMark;
+        private System.Windows.Forms.Button buttonUpdateMark;
+        private System.Windows.Forms.Button buttonDeleteMark;
+        private System.Windows.Forms.Button buttonAddMark;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDownMark;
+        private System.Windows.Forms.ListBox listBoxSchoolObjects;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DataGridView dataGridViewMarks;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDownStudentID;
     }
 }
 
--- ClassRoomApp/Main.cs
+++ ClassRoomApp/Main.cs
@@ -1,27 +1,21 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ClassRoomApp {
     public partial class Main : Form {
         public Main() {
             InitializeComponent();
-            updateStudentInfo();
-        }
+            UpdateStudentInfo();
+			UpdateMarksInfo();
+		}
 		private void exportData(DataGridView dataGrid) {
 			Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
 			Microsoft.Office.Interop.Excel.Workbook ExcelWorkBook;
 			Microsoft.Office.Interop.Excel.Worksheet ExcelWorkSheet;
 			ExcelWorkBook = ExcelApp.Workbooks.Add(System.Reflection.Missing.Value);
 			ExcelWorkSheet = ExcelWorkBook.Worksheets.get_Item(1) as Microsoft.Office.Interop.Excel.Worksheet;
-
 			for (int i = 0; i < dataGrid.Rows.Count; i++)
 			{
 				for (int j = 0; j < dataGrid.ColumnCount; j++)
@@ -32,7 +26,7 @@ namespace ClassRoomApp {
 			ExcelApp.Visible = true;
 			ExcelApp.UserControl = true;
 		}
-		private void updateStudentInfo() {
+		private void UpdateStudentInfo() {
 			ConnectionClass connection = new ConnectionClass();
 			MySqlCommand mySqlCommand = new MySqlCommand("SELECT * from `students`,`classes` where `students`.classid = `classes`.id", connection.GetConnection());
 			try {
@@ -45,8 +39,7 @@ namespace ClassRoomApp {
 				dataGridViewStudent.Columns.Add("birthday", "Дата рождения");
 				dataGridViewStudent.Columns.Add("name", "Класс");
 				dataGridViewStudent.Columns.Add("nubmerPhone", "Номер телефона");
-				while (mySqlDataReader.Read())
-				{
+				while (mySqlDataReader.Read()) {
 					dataGridViewStudent.Rows.Add(mySqlDataReader["id"].ToString(), mySqlDataReader["firstname"].ToString(), mySqlDataReader["secondname"].ToString(), 
 						mySqlDataReader["thirdname"].ToString(), mySqlDataReader["birthday"].ToString(),
 						mySqlDataReader["name"].ToString(), mySqlDataReader["nubmerPhone"].ToString());
@@ -59,6 +52,29 @@ namespace ClassRoomApp {
 				connection.CloseConnection();
 			}
 		}
+		private void UpdateMarksInfo()
+        {
+			ConnectionClass connection = new ConnectionClass();
+			MySqlCommand mySqlCommand = new MySqlCommand("SELECT * from `marks`,`shcoolsubjects`,`students` where `shcoolsubjects`.id = `marks`.subjectid and `students`.id=`marks`.studentid;", connection.GetConnection());
+            try {
+				connection.OpenConnection();
+				MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+				dataGridViewMarks.Columns.Add("id", "Идентификатор оценки");
+				dataGridViewMarks.Columns.Add("firstname", "Имя");
+				dataGridViewMarks.Columns.Add("secondname", "Фамилия");
+				dataGridViewMarks.Columns.Add("thirdname", "Отчество");
+				dataGridViewMarks.Columns.Add("name", "Предмет");
+				dataGridViewMarks.Columns.Add("mark", "Оценка");
+				while (mySqlDataReader.Read()) {
+					dataGridViewMarks.Rows.Add(mySqlDataReader["id"].ToString(), mySqlDataReader["firstname"].ToString(), mySqlDataReader["secondname"].ToString(), mySqlDataReader["thirdname"].ToString(),
+						 mySqlDataReader["name"].ToString(), mySqlDataReader["mark"].ToString());
+				}
+			} catch (Exception ex){
+				MessageBox.Show(ex.ToString(),"Ошибка");
+			} finally {
+				connection.CloseConnection();
+            }
+		}
 		private void buttonStudentUpdate_Click(object sender, EventArgs e) {
 			dataGridViewStudent.Columns.Remove("id"); 
 			dataGridViewStudent.Columns.Remove("firstname"); 
@@ -67,19 +83,15 @@ namespace ClassRoomApp {
 			dataGridViewStudent.Columns.Remove("birthday");
 			dataGridViewStudent.Columns.Remove("name");
 			dataGridViewStudent.Columns.Remove("nubmerPhone");
-			updateStudentInfo();
+			UpdateStudentInfo();
 			MessageBox.Show("Информация успешно обновлена","Информация");
 		}
-
         private void buttonStudentAdd_Click(object sender, EventArgs e)
         {
 			ConnectionClass connection = new ConnectionClass();
-
 			dateBirthd.CustomFormat = "yyyy-MM-dd";
 			dateBirthd.Format = DateTimePickerFormat.Custom;
-
 			int ClassID = 0;
-
 			switch(listBoxClass.SelectedItem) {
 				case "1 А":
 					ClassID = 1;
@@ -166,14 +178,11 @@ namespace ClassRoomApp {
 					ClassID = 28;
 					break;
 			}
-
 			try
 			{
 				connection.OpenConnection();
-
 				string command_string = "INSERT INTO `db`.`students` (`firstname`, `secondname`, `thirdname`, `Birthday`, `classid`, `nubmerPhone`) " +
 					"VALUES (@firstname, @secondname, @thirdname, @Birthday, @classid, @nubmerPhone);";
-
 				MySqlCommand mySqlCommand = new MySqlCommand(command_string, connection.GetConnection());
 					mySqlCommand.Parameters.Add("@firstname", MySqlDbType.VarChar).Value = textBoxName.Text;
 					mySqlCommand.Parameters.Add("@secondname", MySqlDbType.VarChar).Value = textBoxSurname.Text;
@@ -181,7 +190,6 @@ namespace ClassRoomApp {
 					mySqlCommand.Parameters.Add("@Birthday", MySqlDbType.Date).Value = dateBirthd.Value;
 					mySqlCommand.Parameters.Add("@classid", MySqlDbType.Int32).Value = Convert.ToInt32(ClassID);
 					mySqlCommand.Parameters.Add("@nubmerPhone", MySqlDbType.VarChar).Value = textBoxPhone.Text;
-
 				if (mySqlCommand.ExecuteNonQuery() == 1) 
 					MessageBox.Show("Запись успешно добавлена", "Успешно");
 				else 
@@ -194,41 +202,161 @@ namespace ClassRoomApp {
 			finally {
 				connection.CloseConnection();
 			}
-
 		}
-
 		private void buttonDeleteStudent_Click(object sender, EventArgs e)
         {
 			ConnectionClass connection = new ConnectionClass();
-
-			try
-			{
+			try {
 				connection.OpenConnection();
-
 				string command_string = "DELETE FROM `db`.`students` WHERE (`ID` = @ID);";
-
 				MySqlCommand mySqlCommand = new MySqlCommand(command_string, connection.GetConnection());
-				mySqlCommand.Parameters.Add("@ID", MySqlDbType.VarChar).Value = Convert.ToInt32(textBoxStudentID.Text);
-					
+				mySqlCommand.Parameters.Add("@ID", MySqlDbType.VarChar).Value = Convert.ToInt32(textBoxStudentID.Text);		
 				if (mySqlCommand.ExecuteNonQuery() == 1)
 					MessageBox.Show("Запись успешно удалена", "Успешно");
 				else
 					MessageBox.Show("Что-то пошло не так!", "Ошибка!");
-
 			}
-			catch (Exception ex)
-			{
+			catch (Exception ex) {
 				MessageBox.Show(ex.ToString());
 			}
-			finally
-			{
+			finally {
 				connection.CloseConnection();
 			}
 		}
-
-        private void buttonExportStudents_Click(object sender, EventArgs e)
-        {
+        private void buttonExportStudents_Click(object sender, EventArgs e) {
 			exportData(dataGridViewStudent);
 		}
-	}
+        private void buttonAddMark_Click(object sender, EventArgs e)
+        {
+			int subjectId;
+			ConnectionClass connection = new ConnectionClass();
+			DataTable dataTable = new DataTable();
+			MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+			switch (listBoxSchoolObjects.SelectedItem) {
+				case "Русский язык":
+					subjectId = 1;
+					break;
+				case "Литература":
+					subjectId = 2;
+					break;
+				case "Математика":
+					subjectId = 3;
+					break;
+				case "Информатика":
+					subjectId = 4;
+					break;
+				case "Алгебра":
+					subjectId = 5;
+					break;
+				case "Геометрия":
+					subjectId = 6;
+					break;
+				case "Черчение":
+					subjectId = 7;
+					break;
+				case "ИЗО":
+					subjectId = 8;
+					break;
+				case "Биология":
+					subjectId = 9;
+					break;
+				case "Физкультура":
+					subjectId = 10;
+					break;
+				case "История России":
+					subjectId = 11;
+					break;
+				case "Всеобщая история":
+					subjectId = 12;
+					break;
+				case "Астрономия":
+					subjectId = 13;
+					break;
+				case "Физика":
+					subjectId = 14;
+					break;
+				case "География":
+					subjectId = 15;
+					break;
+				case "Иностранный язык":
+					subjectId = 16;
+					break;
+				case "Технология":
+					subjectId = 17;
+					break;
+				case "Химия":
+					subjectId = 18;
+					break;
+				case "Музыка":
+					subjectId = 19;
+					break;
+				case "ОБЖ":
+					subjectId = 20;
+					break;
+				default:
+					subjectId = 0;
+					break;
+			}
+			object StudentId;
+			string command_string_id = "SELECT id from `students` WHERE `id`=@id;";
+			MySqlCommand sqlCommand = new MySqlCommand(command_string_id, connection.GetConnection());
+			sqlCommand.Parameters.Add("@id", MySqlDbType.Int32).Value = numericUpDownStudentID.Value;
+			try {
+				connection.OpenConnection();
+				dataAdapter.SelectCommand = sqlCommand;
+				dataAdapter.Fill(dataTable);
+				if (dataTable.Rows.Count == 1) {
+					StudentId = dataTable.Rows[0]["id"].ToString();
+					string command_string = "INSERT INTO `db`.`marks` (`Mark`, `StudentID`, `SubjectID`) VALUES (@mark, @studentid, @subjectid);";
+					MySqlCommand mySqlCommand = new MySqlCommand(command_string, connection.GetConnection());
+					mySqlCommand.Parameters.Add("@mark", MySqlDbType.Int32).Value = numericUpDownMark.Value;
+					mySqlCommand.Parameters.Add("@studentid", MySqlDbType.Int32).Value = StudentId;
+					mySqlCommand.Parameters.Add("@subjectid", MySqlDbType.Int32).Value = subjectId;
+					if (mySqlCommand.ExecuteNonQuery() == 1) 
+						MessageBox.Show("Запись успешно добавлена", "Успешно");
+					else 
+						MessageBox.Show("Что-то пошло не так!", "Ошибка!");
+				}
+			}
+			catch (Exception ex) {
+				MessageBox.Show(ex.ToString());
+			}
+			finally {
+				connection.CloseConnection();
+			}
+		}
+		private void buttonDeleteMark_Click(object sender, EventArgs e)
+        {
+			ConnectionClass connection = new ConnectionClass();
+			try {
+				connection.OpenConnection();
+				string command_string = "DELETE FROM `db`.`marks` WHERE (`ID` = @ID);";
+				MySqlCommand mySqlCommand = new MySqlCommand(command_string, connection.GetConnection());
+				mySqlCommand.Parameters.Add("@ID", MySqlDbType.VarChar).Value = Convert.ToInt32(numericUpDownStudentID.Text);
+				if (mySqlCommand.ExecuteNonQuery() == 1)
+					MessageBox.Show("Запись успешно удалена", "Успешно");
+				else
+					MessageBox.Show("Что-то пошло не так!", "Ошибка!");
+			}
+			catch (Exception ex) {
+				MessageBox.Show(ex.ToString());
+			}
+			finally {
+				connection.CloseConnection();
+			}
+		}
+        private void buttonExportMark_Click(object sender, EventArgs e) {
+			exportData(dataGridViewMarks);
+        }
+
+        private void buttonUpdateMark_Click(object sender, EventArgs e) {
+			dataGridViewMarks.Columns.Remove("id");
+			dataGridViewMarks.Columns.Remove("firstname");
+			dataGridViewMarks.Columns.Remove("secondname");
+			dataGridViewMarks.Columns.Remove("thirdname");
+			dataGridViewMarks.Columns.Remove("name");
+			dataGridViewMarks.Columns.Remove("mark");
+			UpdateMarksInfo();
+		}
+    }
 }
